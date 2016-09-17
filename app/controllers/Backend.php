4d213b7a959b9bd9d7e29e294406248769de2678
thefx <fenixq@mail.ru>
2016-09-17 11:39:05 +0300

    add edit option

--- app/controllers/Backend.php
+++ app/controllers/Backend.php
@@ -19,13 +19,61 @@ class Backend extends Controller {
         $f3->set('inc','v_admin.htm');
     }
 
+    /**
+     * @param $f3
+     */
+    function editRecord($f3)
+    {
+        $record=new \DB\SQL\Mapper($this->db, 'records');
+        $record->load(array('record_id=?', array($f3->get('GET.id'))));
+
+        if ($record->dry())
+            $f3->reroute('/admin');
+
+        $record->copyto('post');
+
+        $f3->set('title', 'Редактировать запись');
+        $f3->set('inc','v_edit_record.htm');
+    }
+
+    /**
+     * @param $f3
+     */
+    function saveRecord($f3)
+    {
+        $record = new \DB\SQL\Mapper($this->db, 'records');
+        $record->load(array('record_id=?', array($f3->get('GET.id'))));
+
+        if ($record->dry())
+            $f3->reroute('/admin');
+
+        $record->copyfrom($f3->get('POST'));
+        $record->save();
+        $f3->reroute('/admin');
+    }
+
+    /**
+     * @param $f3
+     */
     function delRecord($f3)
     {
+
         if ($f3->get('POST'))
         {
-            $this->db->exec('DELETE FROM records WHERE record_id = ?;', array($f3->get('POST.delRecord')));
+            $record = new \DB\SQL\Mapper($this->db, $this->table);
+
+            $record->load(array('record_id=?', array($f3->get('GET.id'))));
+
+            if (!$record->dry())
+            {
+                $record->erase();
+            }
         }
 
         $f3->reroute();
     }
+
+
+
+
 }
